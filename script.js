eb891f154ca9dde72ddf90e71df54c08552c2ff9
Anup Kumar Mridha <66965208+anupkumarmridha@users.noreply.github.com>
2022-10-24 01:03:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,17 +24,3 @@ response.then((v) => {
         cardContainer.innerHTML = ihtml
 })
 
-/* ******************* NOTES APP (REMAINING QUESTIONS OF PRACTICE SET) *********** */
-// let n = localStorage.getItem("note")
-// alert("Your note is " + n)
-
-// let a = prompt("Enter your note")
-// if (a) {
-//         localStorage.setItem("note", a)
-// }
-
-// let c = confirm("Do you want to delete your note?")
-// if (c) {
-//         localStorage.removeItem("note")
-//         alert("Note deleted successfully!")
-// }
